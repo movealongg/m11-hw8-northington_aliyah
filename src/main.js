692e02ea54ef91b8edc376fab1dc4e0ab1950324
Aliyah Northington <alierin@aliyahs-mbp.attlocal.net>
2018-11-01 18:58:47 -0500

    Add missing JS code to fix errors

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 class Desserts {
 	constructor(type, calories){
-		
+		this.type = type;
+		this.calories = calories;
 	}
 }
 
 class IceCream extends Desserts{
 	constructor(type, calories, flavor, scoops){
-	
+		super(type, calories);
+		this.flavor = flavor;
+		this.scoops = scoops;
 	}
 	
 	includeSpoon(){
