f0230c34c48ac3a5b135793c363dfcf9173963db
Aliyah Northington <alierin@aliyahs-mbp.attlocal.net>
2018-11-01 18:47:07 -0500

    Create method includeSpoon
    This will create method includeSpoon into IceCream sub class.
    
    It will also create an if statement to see if this.scoops is equal to
    the number 3.
    
    If it is, it will log "Here is your spoon!" to the console.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ class Desserts {
 
 class IceCream extends Desserts{
 	constructor(type, calories, flavor, scoops){
-		
+	
+	}
+	
+	includeSpoon(){
+		if(this.scoops=== 3){
+			console.log('Here is your spoon!');
+		}
 	}
 }
